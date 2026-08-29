1e0b1c379f479e069c4dc36e0f5f26ae93c33eed
Abhishek <abhichavan857@gmail.com>
2026-08-29 10:43:51 +0530

    Add HTTP server to application

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,27 @@
 package com.example;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI!");
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(8081), 0
+        );
+
+        server.createContext("/", exchange -> {
+            String response = "Hello from Jenkins + Docker CI/CD!";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+
+        server.start();
+
+        System.out.println("Application started on port 8081");
     }
 
     public static int add(int a, int b) {
